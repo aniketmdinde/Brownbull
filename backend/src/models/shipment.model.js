62416cf653cbdcc99c546680549a5335c56d3db9
aniketmdinde <aniketmdinde@gmail.com>
2025-04-03 01:24:52 +0530

    initial commit

---
+++ backend/src/models/shipment.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const packageSchema = new mongoose.Schema({
+    type: { type: String, enum: ["box", "container"], required: true },
+    dimensions: {
+        length: { type: Number, required: true },
+        width: { type: Number, required: true },
+        height: { type: Number, required: true },
+    },
+    weight: { type: Number, required: true },
+    units: { type: Number, required: true },
+});
+
+const shipmentSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    client_company: { type: String, required: true },
+    pickup_location: { type: String, required: true },
+    drop_location: { type: String, required: true },
+    transport_mode: { type: String, enum: ["air", "land", "sea"], required: true },
+    packages: [packageSchema],
+    shipment_date: { type: Date, required: true },
+    status: { type: String, enum: ["in progress", "delivered", "lost"], default: "in progress" },
+});
+
+module.exports = mongoose.model("Shipment", shipmentSchema);
